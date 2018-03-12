f1073435d2db21e197bfb0aa7d27ad92a0c2f4a2
Mariko <marikoanderson@gmail.com>
2018-03-12 12:35:09 -0700

    updated block class

--- lib/block.rb
+++ lib/block.rb
@@ -1,12 +1,23 @@
-# require_relative 'reception'
+ require_relative 'reception'
 
 require 'pry'
 require 'date'
 
-module Hotel
-  class Block < reception
+# block is-a reception item
+# block inherits from reception
 
-    def initialize
+module Hotel
+  class Block < Reception
+
+    def initialize(check_in, check_out, number_of_rooms)
+      @check_in = check_in
+      @check_out = check_out
+      @number_of_rooms = number_of_rooms
+
+      availability_by_date_range(check_in, check_out)
+    end
+
+    def hold_block
 
     end
   end
